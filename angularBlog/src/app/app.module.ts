cfa6c588520c85ee16a95495375018099219a3d0
Robin Magnevik <robin.mangevik@gmail.com>
2020-03-11 13:14:01 +0100

    created components, services and interface

--- angularBlog/src/app/app.module.ts
+++ angularBlog/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { LatestArticlesComponent } from './latest-articles/latest-articles.component';
+import { AllArticlesComponent } from './all-articles/all-articles.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { AddArticleComponent } from './add-article/add-article.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    LatestArticlesComponent,
+    AllArticlesComponent,
+    LogInComponent,
+    AddArticleComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
