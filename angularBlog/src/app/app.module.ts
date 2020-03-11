c20239e6919216dd11284d92bb6bf127de4a1fbb
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-03-11 14:11:02 +0100

    amended in app module

--- angularBlog/src/app/app.module.ts
+++ angularBlog/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +27,6 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
