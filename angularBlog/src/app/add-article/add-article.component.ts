b1684d85b516990b74c37ca56c963ba5f5b049bc
Robin Magnevik <robin.mangevik@gmail.com>
2020-03-12 09:02:29 +0100

    hej

--- angularBlog/src/app/add-article/add-article.component.ts
+++ angularBlog/src/app/add-article/add-article.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleClass } from '../article-class';
+import { ArticleDataService } from '../article-data.service'
+import { RangeValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-add-article',
@@ -7,15 +9,35 @@ import { ArticleClass } from '../article-class';
   styleUrls: ['./add-article.component.css']
 })
 export class AddArticleComponent implements OnInit {
-
   userModel = new ArticleClass("", "", "", null)
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
   onSubmit() {
     console.log(this.userModel);
+  }
+  newArticle: ArticleClass;
+  articleTitle:string;
+  articleContent:string;
+  articleWriter:string;
+  articleTime:number;
+
+  constructor(public articleDataService: ArticleDataService) { }
+
+  ngOnInit(): void {
+    this.newArticle = new ArticleClass('', '', '', 0);
+  }
+
+  title(event){
+    this.articleTitle = event.target.value;
+  }
+  content(event){
+    this.articleContent = event.target.value;
+  }
+  writer(event){
+    this.articleWriter = event.target.value;
+  }
+  time(event){
+    this.articleTime = event.target.value;
+  }
+  addNewArticle(){
+    this.articleDataService.newArticleAdd(this.articleTitle, this.articleContent, this.articleWriter,this.articleTime)
   }
 }
